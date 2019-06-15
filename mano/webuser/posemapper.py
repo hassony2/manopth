c0af7d5c043cb7729f36a6667f3354d3226b2119
Yana <yanahasson@gmail.com>
2019-06-15 18:06:40 +0200

    Add header back to posemapper

--- mano/webuser/posemapper.py
+++ mano/webuser/posemapper.py
@@ -1,3 +1,24 @@
+'''
+Copyright 2017 Javier Romero, Dimitrios Tzionas, Michael J Black and the Max Planck Gesellschaft.  All rights reserved.
+This software is provided for research purposes only.
+By using this software you agree to the terms of the MANO/SMPL+H Model license here http://mano.is.tue.mpg.de/license
+
+More information about MANO/SMPL+H is available at http://mano.is.tue.mpg.de.
+For comments or questions, please email us at: mano@tue.mpg.de
+
+
+About this file:
+================
+This file defines a wrapper for the loading functions of the MANO model.
+
+Modules included:
+- load_model:
+  loads the MANO model from a given file location (i.e. a .pkl file location),
+  or a dictionary object.
+
+'''
+
+
 import chumpy as ch
 import numpy as np
 import cv2
